b776d53f09edab4a2a6e9ceb3913eced6f22e30a
Falcon <yuutku294193@gmail.com>
2023-01-10 17:43:03 +0700

    Update

---
+++ garbi_contracts/contracts/info/BalanceInfo.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma experimental ABIEncoderV2;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+contract BalanceInfo {
+        struct TokenBALANCE {
+                address token;
+                uint256 amount;
+        }
+	function getData(address _account, IBEP20[] memory _tokens) public view returns(TokenBALANCE[] memory tokensBal_, uint256 nativeBal_)
+	{
+		nativeBal_ = address(_account).balance;
+		tokensBal_ = new TokenBALANCE[](_tokens.length);
+		for (uint256 idx = 0; idx < _tokens.length; idx++) {
+			tokensBal_[idx].token = address(_tokens[idx]);
+			tokensBal_[idx].amount = _tokens[idx].balanceOf(_account);
+		}
+	}
+}
