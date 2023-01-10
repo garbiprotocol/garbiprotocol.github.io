989084f448bd9a606e09552eeda9569fb92fa148
Falcon <yuutku294193@gmail.com>
2023-01-10 17:09:51 +0700

    UpdatE

--- garbi_contracts/migrations/1_deploy_contracts.js
+++ garbi_contracts/migrations/1_deploy_contracts.js
@@ -3,9 +3,11 @@ const GRB = artifacts.require("GRB");
 const GRBProxy = artifacts.require("GRBProxy");
 const GDAI = artifacts.require("GDAI");
 const GUSDC = artifacts.require("GUSDC");
+const GarbiswapTradeInfo = artifacts.require("GarbiswapTradeInfo");
 
 module.exports = function(deployer) {
-    deployer.deploy(GDAI, "1000000000000000000000000", "500000000000000000000000");
-    deployer.deploy(GUSDC, "1000000000000000000000000", "500000000000000000000000");
+    // deployer.deploy(GDAI, "1000000000000000000000000", "500000000000000000000000");
+    // deployer.deploy(GUSDC, "1000000000000000000000000", "500000000000000000000000");
     //deployer.deploy(GRBProxy, "0x5fd71280b6385157b291b9962f22153fc9e79000");
+    deployer.deploy(GarbiswapTradeInfo);
 };
