02e487b8c14d4f0b28e9f0cf25e83c4cac018f15
CoinHunter314159 <coinhunter314159@gmail.com>
2023-01-27 09:19:25 +0700

    update

---
+++ garbi_contracts/contracts/GarbiVestGRB.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+import './interfaces/IGarbiTimeLock.sol';
+import './interfaces/IGarbiswapWhitelist.sol';
+import './interfaces/IERC20withBurn.sol';
+
+contract GarbiVestGRB is ReentrancyGuard, Ownable {
+
+    using SafeMath for uint256;
+
+    IERC20 public GRB;
+    IERC20withBurn public veGRB;
+
+    IGarbiTimeLock public garbiTimeLockContract;
+    IGarbiswapWhitelist public whitelistContract;
+
+    mapping(address => uint256) public userVeGRBDeposited;
+    mapping(address => uint256) public userGRBAvailable;
+    mapping(address => uint256) public userVeGRBConvertToGRB;
+    mapping(address => uint256) public userLastVestCaculated;
+    mapping(address => uint256) public userVestFinished;
+
+    uint256 public PERCENTAGE_GRB = 40; //40% 40/100
+    uint256 public totalBlock = 5760;
+
+    event onDeposit(address _user, uint256 _amount);
+    event onClaim(address _user, uint256 _amount);
+
+    modifier onlyWhitelist()
+    {
+        if (msg.sender != tx.origin) {
+            require(whitelistContract.whitelisted(msg.sender) == true, 'INVALID_WHITELIST');
+        }
+        _;
+    }
+
+    constructor(
+        IERC20 _grb,
+        IERC20withBurn _veGrb,
+        IGarbiswapWhitelist _whitelistContract,
+        IGarbiTimeLock _garbiTimeLockContract
+    ) {
+        GRB = _grb;
+        veGRB = _veGrb;
+        garbiTimeLockContract = _garbiTimeLockContract;
+        whitelistContract = _whitelistContract;
+    }
+
+    function setWhitelistContract() public onlyOwner {
+        require(garbiTimeLockContract.isQueuedTransaction(address(this), 'setWhitelistContract'), "INVALID_PERMISSION");
+
+        address _whitelistContract = garbiTimeLockContract.getAddressChangeOnTimeLock(address(this), 'setWhitelistContract', 'whitelistContract');
+
+        require(_whitelistContract != address(0), "INVALID_ADDRESS");
+
+        whitelistContract = IGarbiswapWhitelist(_whitelistContract);
+
+        garbiTimeLockContract.clearFieldValue('setWhitelistContract', 'whitelistContract', 1);
+        garbiTimeLockContract.doneTransactions('setWhitelistContract');
+    }
+
+    function deposit(uint256 _veGRBAmount) public onlyWhitelist nonReentrant {
+        require(_veGRBAmount > 0, 'INVALID_INPUT');
+        
+        updateUser(msg.sender);
+
+        uint256 userVeGRBBalance = veGRB.balanceOf(msg.sender);
+        if(_veGRBAmount > userVeGRBBalance) {
+            _veGRBAmount = userVeGRBBalance;
+        }
+
+        veGRB.transferFrom(msg.sender, address(this), _veGRBAmount);
+
+        userVeGRBDeposited[msg.sender] = userVeGRBDeposited[msg.sender].add(_veGRBAmount).sub(userVeGRBConvertToGRB[msg.sender]);
+        userVeGRBConvertToGRB[msg.sender] = 0;
+
+        uint256 availableGRB = _veGRBAmount.mul(PERCENTAGE_GRB).div(100);
+        userGRBAvailable[msg.sender] = userGRBAvailable[msg.sender].add(availableGRB);
+        userVeGRBDeposited[msg.sender] = userVeGRBDeposited[msg.sender].sub(availableGRB);
+
+        userVestFinished[msg.sender] = block.number + totalBlock;
+
+        emit onDeposit(msg.sender, _veGRBAmount);
+    }
+
+    function claimGRB(uint256 _amount) public onlyWhitelist nonReentrant {
+        require(_amount > 0, 'INVALID_INPUT');
+        
+        updateUser(msg.sender);
+
+        uint256 vaultBalance = GRB.balanceOf(address(this));
+        
+        if(_amount > vaultBalance) {
+            _amount = vaultBalance;
+        }
+
+        if(_amount > userGRBAvailable[msg.sender]) {
+            _amount = userGRBAvailable[msg.sender];
+        }
+        
+        userGRBAvailable[msg.sender] = userGRBAvailable[msg.sender].sub(_amount);  
+        GRB.transfer(msg.sender, _amount);
+        veGRB.burn(_amount);
+
+        emit onClaim(msg.sender, _amount);
+    }
+
+    function updateUser(address _user) public {
+        uint256 pendingGRB = getUserPendingGRB(_user);
+        userVeGRBConvertToGRB[_user] = userVeGRBConvertToGRB[_user].add(pendingGRB);
+        userGRBAvailable[_user] = userGRBAvailable[_user] + pendingGRB;
+        userLastVestCaculated[_user] = block.number;
+    }
+
+    function getUserVeGRBBalance(address _user) public view returns(uint256 userVeGRBBalance) {
+        userVeGRBBalance = userVeGRBDeposited[_user].sub(userVeGRBConvertToGRB[_user]).sub(getUserPendingGRB(_user));
+    }
+
+    function getUserPendingGRB(address _user) public view returns(uint256) {
+        uint256 userVestingSpeed = userVeGRBDeposited[_user].div(totalBlock);
+        uint256 userPendingGRB = 0;
+        if(block.number <= userVestFinished[_user]) {
+            userPendingGRB = userVestingSpeed.mul(getBlockFrom(userLastVestCaculated[_user], block.number));
+            return userPendingGRB;
+        }
+        else {
+            if(userLastVestCaculated[_user] > userVestFinished[_user]) {
+                return 0;
+            }
+            else {
+                userPendingGRB = userVestingSpeed.mul(getBlockFrom(userLastVestCaculated[_user], userVestFinished[_user]));
+                return userPendingGRB;
+            }
+        }
+    }
+
+    function getBlockFrom(uint256 _from, uint256 _to) public pure returns (uint256) {
+        return _to.sub(_from);
+    }
+
+    function getUserInfo(address _user) public view returns(uint256 userVeGRBBalance, uint256 userGRBCanClaim, uint256 vestFinished, uint256 lastVestCaculated, uint256 veGRBDeposited, uint256 blocknow) {
+        userVeGRBBalance = getUserVeGRBBalance(_user);
+        userGRBCanClaim = userGRBAvailable[_user] + getUserPendingGRB(_user);
+        vestFinished = userVestFinished[_user];
+        lastVestCaculated = userLastVestCaculated[_user];
+        veGRBDeposited = userVeGRBDeposited[_user];
+        blocknow = block.number;
+    }
+
+}
--- garbi_contracts/migrations/1_libs_deploy_contracts.js
+++ garbi_contracts/migrations/1_libs_deploy_contracts.js
@@ -7,10 +7,19 @@ const GarbiOracle = artifacts.require("GarbiOracle");
 const GarbiMining = artifacts.require("GarbiMining");
 const GarbiPlatformFund = artifacts.require("GarbiPlatformFund");
 const GarbiPerformanceFund = artifacts.require("GarbiPerformanceFund");
+const GarbiVestGRB = artifacts.require("GarbiVestGRB");
 
-const GRB = "0x5Fd71280b6385157b291b9962f22153FC9E79000";
-const performanceMachineContract = "0xd9d820dd337fa2da2edef1ee240cc236dbc23359";
-const veGRB = "0x14c302dca44528a2b00b932afdf01e9d48100b7b";
+//production
+//const GRB = "0x5Fd71280b6385157b291b9962f22153FC9E79000";
+//const performanceMachineContract = "0xd9d820dd337fa2da2edef1ee240cc236dbc23359";
+//const veGRB = "0x14c302dca44528a2b00b932afdf01e9d48100b7b";
+//end production
+//testnet
+const GRB = "0xd1eb8a5798e04703ec5324c99405f581bd6318b8";
+const veGRB = "0xcf7d3a1ff5188a0d398cf8181b8bdc051204f8da";
+const GarbiTimeLockAddress = "0x3e0b1ae5edd611eb6b9e22c2ad737b8453e7682d";
+const GarbiswapWhitelistAddress = "0x3bafb09dd2fe339021b812ef230db546d600479e";
+//end testnet
 const startBlock = 1;
 
 module.exports = function(deployer) {
@@ -32,4 +41,10 @@ module.exports = function(deployer) {
 //    deployer.deploy(GarbiPlatformFund);
 //    deployer.deploy(GarbiswapWhitelist);
 //    deployer.deploy(GarbiOracle);
+    deployer.deploy(GarbiVestGRB,
+                    GRB,
+                    veGRB, 
+                    GarbiswapWhitelistAddress,
+                    GarbiTimeLockAddress,
+                    );        
 };
