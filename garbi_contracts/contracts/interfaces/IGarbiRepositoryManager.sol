21f381e65e5bfc28fb1e4c809418ac99aeaccd77
CoinHunter314159 <coinhunter314159@gmail.com>
2023-03-02 16:22:29 +0700

    update

--- garbi_contracts/contracts/interfaces/IGarbiRepositoryManager.sol
+++ garbi_contracts/contracts/interfaces/IGarbiRepositoryManager.sol
@@ -4,4 +4,6 @@ pragma solidity >=0.6.12;
 interface IGarbiRepositoryManager {
     function repoAddresses(uint256) external view returns(address);
     function getGarbiECPrice() external view returns(uint256);
+    function getDataToSellGarbiEC(address repoOutAddress, uint256 garbiECInAmount) external view returns(uint256);
+    function getFeeWithOutAmount(address repoOutAddress, uint256 assetOutAmount) external view returns(uint256);
 }
