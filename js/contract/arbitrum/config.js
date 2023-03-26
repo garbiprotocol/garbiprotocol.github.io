8518e605d9657c083343413167ce4ee091f8ac9b
tusgrace1306 <tusgrace1306@gmail.com>
2023-03-26 12:32:02 +0700

    upate contract grb farm uni

--- js/contract/arbitrum/config.js
+++ js/contract/arbitrum/config.js
@@ -276,7 +276,7 @@ $.CONFIG.prototype = (function() {
                 contract: "0x622e4726a167799826d1E1D150b076A7725f5D81"
             },
             GarbiFarmUniV3: {
-                contract: "0xB11ef8B1871A1b4308eaf0d8ce47538a04cdE300"
+                contract: "0x0E8A3830581e52Ca678cFDB0622b2795Bc214490"
             },
             garbiRepository: {
                 'dai': '0x508C16e20C4d3730570C7b9B73e1c88538dA8a26',
