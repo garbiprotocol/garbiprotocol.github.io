63159a32c61d4fecef7e2c8d84d014132354ab99
tusgrace1306 <tusgrace1306@gmail.com>
2023-03-28 14:56:41 +0700

    update

--- js/contract/arbitrum/config.js
+++ js/contract/arbitrum/config.js
@@ -276,7 +276,7 @@ $.CONFIG.prototype = (function() {
                 contract: "0x622e4726a167799826d1E1D150b076A7725f5D81"
             },
             GarbiFarmUniV3: {
-                contract: "0x7F702828817314c6ab0707E70992308fDEbA025d"
+                contract: "0x702bFF013041c485039172fD8aB2c173ad0f6771"
             },
             garbiRepository: {
                 'dai': '0x508C16e20C4d3730570C7b9B73e1c88538dA8a26',
