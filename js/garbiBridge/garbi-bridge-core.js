227eb58a55b3d5432d1a12b2d0345ac7e4bd7a97
Shiro Izanagi <shiroizanagi88@gmail.com>
2023-05-23 14:49:56 +0700

    update

--- js/garbiBridge/garbi-bridge-core.js
+++ js/garbiBridge/garbi-bridge-core.js
@@ -1,30 +1,29 @@
 $.GARBI_BRIDGE = function() {};
-$.GARBI_BRIDGE.prototype = (function()
-{
+$.GARBI_BRIDGE.prototype = (function() {
     var setting = {
         chainId: 56,
     };
 
     return {
-        init: function(options)
-        {
+        init: function(options) {
             if (typeof options === "undefined" || options.length < 1) {
                 return false;
             }
             setting = $.extend({}, setting, options);
         },
 
-        async Bridge(networkDeposit, networkRecieve, tokenDeposit, amountInputBridge)
-        {
+        async Bridge(networkDeposit, networkRecieve, tokenDeposit, amountInputBridge) {
             let self = this;
             let user = self.GetWalletAddress();
-            
-            let bridgeToNetwork = garbiBridgeConfig.GetBridgeToNetworkByNetworkName(networkDeposit);
+
+            let bridgeFromNetwork = garbiBridgeConfig.GetBridgeToNetworkByNetworkName(networkDeposit);
+            let resourceID = bridgeFromNetwork.resourceID[tokenDeposit];
+
+            let bridgeToNetwork = garbiBridgeConfig.GetBridgeToNetworkByNetworkName(networkRecieve);
             let destinationDomainID = bridgeToNetwork.domainID;
-            let resourceID = bridgeToNetwork.resourceID[tokenDeposit];
 
             let dataDeposit = self.ConvertDataDepositToBytes(amountInputBridge, user);
-            
+
             let contractConfig = garbiBridgeConfig.GetContractAddressByNetworkName(networkDeposit);
             let brideContractAddress = contractConfig.bridgeContract;
             let bridgeAbi = garbiBridgeAbi.GetBridgeContractABI();
@@ -33,31 +32,30 @@ $.GARBI_BRIDGE.prototype = (function()
 
 
             brideContractAction.methods.deposit(destinationDomainID, resourceID, dataDeposit)
-            .send({from: user, value: 0})
-            .on('transactionHash', (hash) => {
-                coreHelper.showPopup('confirm-popup');
-                $('.transaction-hash').attr("href", "https://arbiscan.io/tx/"+hash);
-            })
-            .on('confirmation', (confirmationNumber, receipt) => {
-                coreHelper.hidePopup('confirm-popup', 0);
-                coreHelper.showPopup('success-confirm-popup');
-                coreHelper.hidePopup('success-confirm-popup', 10000);
-            })
-            .on('receipt', (receipt) => {
-                $('#token-bridge').modal('show');
-                coreHelper.hidePopup('confirm-popup', 0);
-                coreHelper.showPopup('success-confirm-popup');
-                coreHelper.hidePopup('success-confirm-popup', 10000);
+                .send({ from: user, value: 0 })
+                .on('transactionHash', (hash) => {
+                    coreHelper.showPopup('confirm-popup');
+                    $('.transaction-hash').attr("href", "https://arbiscan.io/tx/" + hash);
+                })
+                .on('confirmation', (confirmationNumber, receipt) => {
+                    coreHelper.hidePopup('confirm-popup', 0);
+                    coreHelper.showPopup('success-confirm-popup');
+                    coreHelper.hidePopup('success-confirm-popup', 10000);
+                })
+                .on('receipt', (receipt) => {
+                    $('#token-bridge').modal('show');
+                    coreHelper.hidePopup('confirm-popup', 0);
+                    coreHelper.showPopup('success-confirm-popup');
+                    coreHelper.hidePopup('success-confirm-popup', 10000);
 
-                console.log(receipt);
-            })
-            .on('error', (err, receipt) => {
-                console.log(err);
-            });
+                    console.log(receipt);
+                })
+                .on('error', (err, receipt) => {
+                    console.log(err);
+                });
         },
 
-        async GetAllowanceOfTokenDepositToERC20Handle(chainName, tokenName)
-        {
+        async GetAllowanceOfTokenDepositToERC20Handle(chainName, tokenName) {
             let self = this;
             let user = self.GetWalletAddress();
             let erc20HandleAddress = garbiBridgeConfig.GetERC20HandleAddressByNetworkName(chainName);
@@ -65,8 +63,7 @@ $.GARBI_BRIDGE.prototype = (function()
             return result;
         },
 
-        async ApproveTokenDepositToERC20Handle(chainName, tokenName)
-        {
+        async ApproveTokenDepositToERC20Handle(chainName, tokenName) {
             let self = this;
             let user = self.GetWalletAddress();
             let erc20HandleAddress = garbiBridgeConfig.GetERC20HandleAddressByNetworkName(chainName);
@@ -74,19 +71,16 @@ $.GARBI_BRIDGE.prototype = (function()
             self.ApproveERC20(chainName, tokenName, user, erc20HandleAddress);
         },
 
-        async SwichNetworkDeposit(selectNetwork)
-        {   
+        async SwichNetworkDeposit(selectNetwork) {
             let self = this;
             let networkDeposit = garbiBridgeConfig.GetNetworkByNetworkName(selectNetwork);
 
             let checkNetwork = await self.IsValidNode(networkDeposit.chainId);
-            if(checkNetwork == false)
-            {
+            if (checkNetwork == false) {
                 let provider = window.ethereum;
                 let chainIdToHex = self.ConvertChainIdToHex(networkDeposit.chainId);
-    
-                let networkChain = 
-                [{ 
+
+                let networkChain = [{
                     "chainId": chainIdToHex,
                     "chainName": networkDeposit.chainName,
                     "nativeCurrency": {
@@ -97,36 +91,29 @@ $.GARBI_BRIDGE.prototype = (function()
                     "rpcUrls": networkDeposit.rpcList,
                     "blockExplorerUrls": networkDeposit.blockExplorerUrls,
                 }];
-    
-                try
-                {
-                    await provider.request({ 
-                        "method": 'wallet_addEthereumChain', 
-                        "params": networkChain, 
+
+                try {
+                    await provider.request({
+                        "method": 'wallet_addEthereumChain',
+                        "params": networkChain,
                     });
-                }
-                catch(error)
-                {
-                    await web3.currentProvider.request(
-                    {
+                } catch (error) {
+                    await web3.currentProvider.request({
                         method: 'wallet_switchEthereumChain',
                         params: [{ chainId: chainId }],
-                    }); 
+                    });
                 }
             }
         },
 
-        GetWalletAddress()
-        {
+        GetWalletAddress() {
             let self = this;
-            if(self.CheckConnected() == true)
-            {
+            if (self.CheckConnected() == true) {
                 return web3.currentProvider.selectedAddress;
             }
         },
 
-        async GetAllowance(chainName, tokenName, owner, spender)
-        {
+        async GetAllowance(chainName, tokenName, owner, spender) {
             let self = this;
             let tokenAddress = garbiBridgeConfig.GetTokenAddressByNetworkName(chainName, tokenName);
             let tokenAbi = garbiBridgeAbi.GetERC20BaseABI();
@@ -138,8 +125,7 @@ $.GARBI_BRIDGE.prototype = (function()
             return result;
         },
 
-        async GetBalance(chainName, tokenName, user)
-        {
+        async GetBalance(chainName, tokenName, user) {
             let chainId = garbiBridgeConfig.GetChainIdByNetworkNameToRead(chainName);
             let tokenAddress = garbiBridgeConfig.GetTokenAddressByNetworkName(chainName, tokenName);
             let tokenAbi = garbiBridgeAbi.GetERC20BaseABI();
@@ -149,8 +135,7 @@ $.GARBI_BRIDGE.prototype = (function()
             return result;
         },
 
-        async ApproveERC20(chainName, tokenName, owner, spender)
-        {
+        async ApproveERC20(chainName, tokenName, owner, spender) {
             let self = this;
             let tokenAddress = garbiBridgeConfig.GetTokenAddressByNetworkName(chainName, tokenName);
             let tokenAbi = garbiBridgeAbi.GetERC20BaseABI();
@@ -159,76 +144,66 @@ $.GARBI_BRIDGE.prototype = (function()
 
             let contract = contractBaseHelper.getMainContract(tokenAddress, tokenAbi, chainId);
             contract.methods.approve(spender, amountDefault)
-            .send({from: owner})
-            .on('transactionHash', (hash) => {
-                coreHelper.showPopup('confirm-popup');
-                $('.transaction-hash').attr("href", "https://arbiscan.io/tx/"+hash);
-            })
-            .on('confirmation', (confirmationNumber, receipt) => {
-                coreHelper.hidePopup('confirm-popup', 0);
-                coreHelper.showPopup('success-confirm-popup');
-                coreHelper.hidePopup('success-confirm-popup', 10000);
-            })
-            .on('receipt', (receipt) => {
-                $('.button-approve').hide();
-                coreHelper.hidePopup('confirm-popup', 0);
-                coreHelper.showPopup('success-confirm-popup');
-                coreHelper.hidePopup('success-confirm-popup', 10000);
-            })
-            .on('error', (err, receipt) => {
-                console.log(err);
-            });
+                .send({ from: owner })
+                .on('transactionHash', (hash) => {
+                    coreHelper.showPopup('confirm-popup');
+                    $('.transaction-hash').attr("href", "https://arbiscan.io/tx/" + hash);
+                })
+                .on('confirmation', (confirmationNumber, receipt) => {
+                    coreHelper.hidePopup('confirm-popup', 0);
+                    coreHelper.showPopup('success-confirm-popup');
+                    coreHelper.hidePopup('success-confirm-popup', 10000);
+                })
+                .on('receipt', (receipt) => {
+                    $('.button-approve').hide();
+                    coreHelper.hidePopup('confirm-popup', 0);
+                    coreHelper.showPopup('success-confirm-popup');
+                    coreHelper.hidePopup('success-confirm-popup', 10000);
+                })
+                .on('error', (err, receipt) => {
+                    console.log(err);
+                });
         },
 
-        ConvertDataDepositToBytes(amount, recipient)
-        {
+        ConvertDataDepositToBytes(amount, recipient) {
             let self = this;
             const data = '0x' +
-            ethers.utils.hexZeroPad(ethers.utils.bigNumberify(self.ExpandDecimals(amount)).toHexString(), 32).substr(2) +   
-            ethers.utils.hexZeroPad(ethers.utils.hexlify((recipient.length - 2)/2), 32).substr(2) +   
-            recipient.substr(2);                   
+                ethers.utils.hexZeroPad(ethers.utils.bigNumberify(self.ExpandDecimals(amount)).toHexString(), 32).substr(2) +
+                ethers.utils.hexZeroPad(ethers.utils.hexlify((recipient.length - 2) / 2), 32).substr(2) +
+                recipient.substr(2);
 
             return data;
 
         },
 
-        ExpandDecimals(amount, decimals = 18)
-        {
+        ExpandDecimals(amount, decimals = 18) {
             return ethers.utils.parseUnits(String(amount), String(decimals));
         },
 
-        CheckConnected()
-        {
+        CheckConnected() {
             return web3.currentProvider.selectedAddress !== null ? true : false;
         },
-        
-        async IsValidNode(chainIdNetworkDeposit)
-        {
+
+        async IsValidNode(chainIdNetworkDeposit) {
             let self = this;
             let chainIdActive = await self.GetChainIdToMain();
-            if(chainIdActive != chainIdNetworkDeposit) 
-            {
+            if (chainIdActive != chainIdNetworkDeposit) {
                 return false;
             }
             return true;
         },
 
-        async GetChainIdToMain()
-        {
-            let chainId = await web3.currentProvider.request({ method: 'eth_chainId'});
+        async GetChainIdToMain() {
+            let chainId = await web3.currentProvider.request({ method: 'eth_chainId' });
 
-            if(`${chainId.charAt(0)}${chainId.charAt(1)}` == '0x')
-            {
+            if (`${chainId.charAt(0)}${chainId.charAt(1)}` == '0x') {
                 return parseInt(chainId.slice(2), 16);
-            }
-            else
-            {
+            } else {
                 return parseInt(chainId, 16);
             }
         },
 
-        ConvertChainIdToHex(chainId)
-        {
+        ConvertChainIdToHex(chainId) {
             return `0x${parseInt(chainId, 10).toString(16)}`;
         }
 
